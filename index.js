36bf0734799232cd44fd9b06f9d4ea1edd6cc01f
Sebastian Klose <mail@sklose.com>
2020-11-13 11:08:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ const client = new CosmosClient({ endpoint, key });
  
 async function main() {
   const { database } = await client.databases.createIfNotExists({ id: "Test Database" });
-  console.log(database.id);
+  console.log("created database", database.id);
+ 
+  const { container } = await database.containers.createIfNotExists({ id: "Test Container" });
+  console.log("created container", container.id);
+ 
+  const cities = [
+    { id: "1", name: "Olympia", state: "WA", isCapitol: true },
+    { id: "2", name: "Redmond", state: "WA", isCapitol: false },
+    { id: "3", name: "Chicago", state: "IL", isCapitol: false }
+  ];
+  for (const city of cities) {
+    await container.items.create(city);
+  }
+  console.log("inserted records into container");
+ 
+  const doc = await container.item("1").read();
+  console.log("loaded doc id=1", JSON.stringify(doc));
 }
  
 main().catch((error) => {
