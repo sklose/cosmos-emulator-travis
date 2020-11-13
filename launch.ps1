7643de6385ecf4f2745df07694f48f7024c57693
Plamen Ivanov <plamen.ivanov@jet.com>
2020-11-13 17:04:35 -0500

    remove IP replacement

--- launch.ps1
+++ launch.ps1
@@ -41,9 +41,9 @@ curl 'https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409' -o .\az_storag
 Write-Host "Installing Storage Emulator"
 Start-Process -wait .\az_storage_emulator.msi -ArgumentList "/quiet"
 
-$vm_ip = (Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "IPv4").IPAddress
-$storage_emulator_config_path = "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe.config"
-((Get-Content -path $storage_emulator_config_path -Raw) -replace '127.0.0.1', $vm_ip ) | Set-Content -Path $storage_emulator_config_path
+# $vm_ip = (Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "IPv4").IPAddress
+# $storage_emulator_config_path = "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe.config"
+# ((Get-Content -path $storage_emulator_config_path -Raw) -replace '127.0.0.1', $vm_ip ) | Set-Content -Path $storage_emulator_config_path
 
 # Start-Sleep -s 60
 
