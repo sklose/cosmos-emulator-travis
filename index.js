0846c912a3a612360dd5a2b226f79f4899c8fe03
Sebastian Klose <mail@sklose.com>
2020-11-13 12:06:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { CosmosClient } = require("@azure/cosmos");
  
 const endpoint = "https://localhost:8081";
-const key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+const key = process.env.MASTER_KEY;
 const client = new CosmosClient({ endpoint, key });
  
 async function main() {
