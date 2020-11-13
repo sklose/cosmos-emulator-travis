e48c4e300c9022fa32ef73505db144c4ad870104
Plamen Ivanov <plamen.ivanov@jet.com>
2020-11-13 15:35:57 -0500

    Add SqlLocalDB

--- launch.ps1
+++ launch.ps1
@@ -29,11 +29,29 @@ node index.js
 # Storage Emulator
 
 # Azure storage emulator
+
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+# SQL DB
+curl 'https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SqlLocalDB.MSI' .\SqlLocalDB.MSI
+Start-Process -wait msiexec -ArgumentList "/i",".\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
+
 Write-Host "Downloading Storage Emulator"
 curl 'https://go.microsoft.com/fwlink/?linkid=717179&clcid=0x409' -o .\az_storage_emulator.msi
 
 Write-Host "Installing Storage Emulator"
 Start-Process -wait .\az_storage_emulator.msi -ArgumentList "/quiet"
+
+$vm_ip = (Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "IPv4").IPAddress
+$storage_emulator_config_path = "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe.config"
+((Get-Content -path $storage_emulator_config_path -Raw) -replace '127.0.0.1', $vm_ip ) | Set-Content -Path $storage_emulator_config_path
+
+Start-Sleep -s 60
+
+Write-Host "SqlLocalDB.exe create MSSQLLocalDB"
+Start-Process -wait "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SqlLocalDB.exe" -ArgumentList "create","MSSQLLocalDB"
+
+Write-Host "SqlLocalDB.exe start MSSQLLocalDB"
+Start-Process -wait "C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SqlLocalDB.exe" -ArgumentList "start","MSSQLLocalDB"
 
 Write-Host  "AzureStorageEmulator.exe start"
 Start-Process -wait "C:\Program Files (x86)\Microsoft SDKs\Azure\Storage Emulator\AzureStorageEmulator.exe"  -ArgumentList "start"
