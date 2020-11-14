f946e287dbf111afbd2a7d33515b07b971bbf7ce
Plamen Ivanov <plamen.ivanov@jet.com>
2020-11-13 19:30:26 -0500

    fix RunAs

--- launch.ps1
+++ launch.ps1
@@ -37,7 +37,7 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Write-Host "curl SqlLocalDB.MSI"
 curl 'https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SqlLocalDB.MSI' -o .\SqlLocalDB.MSI
 Write-Host "install SqlLocalDB.MSI"
-Start-Process -Wait -RunAs msiexec -ArgumentList "/i",".\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
+Start-Process -Wait -Verb RunAs msiexec -ArgumentList "/i",".\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
 
 Get-ChildItem -Path "C:\Program Files"
 Get-ChildItem -Path "C:\Program Files\Microsoft SQL Server"
