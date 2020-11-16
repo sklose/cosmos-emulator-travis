70b58fc088973cca8cb329fa16de2e3867db2407
Plamen Ivanov <plamen.ivanov@jet.com>
2020-11-16 17:48:38 -0500

    launch correct js file

--- launch.ps1
+++ launch.ps1
@@ -1,30 +1,30 @@
-# Write-Host "Downloading Cosmos Emulator"
-# curl https://aka.ms/cosmosdb-emulator -o .\cosmos.msi
+Write-Host "Downloading Cosmos Emulator"
+curl https://aka.ms/cosmosdb-emulator -o .\cosmos.msi
 
-# Write-Host "Installing Emulator"
-# Start-Process -Wait .\cosmos.msi -ArgumentList "/quiet"
+Write-Host "Installing Emulator"
+Start-Process -Wait .\cosmos.msi -ArgumentList "/quiet"
 
-# Write-Host "Available Powershell Modules"
-# Get-Module -ListAvailable
+Write-Host "Available Powershell Modules"
+Get-Module -ListAvailable
 
-# Write-Host "Loading CosmosDB Module"
-# Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
+Write-Host "Loading CosmosDB Module"
+Import-Module "$env:ProgramFiles\Azure Cosmos DB Emulator\PSModules\Microsoft.Azure.CosmosDB.Emulator"
 
-# Write-Host "Current Emulator Status"
-# Get-CosmosDbEmulatorStatus
+Write-Host "Current Emulator Status"
+Get-CosmosDbEmulatorStatus
 
-# New-Variable Key -Scope Global -Option Constant -Value "$env:MASTER_KEY"
-# New-Variable Timeout -Scope Global -Option Constant -Value 3600
-# New-Variable PartitionCount -Scope Global -Option Constant -Value 10
+New-Variable Key -Scope Global -Option Constant -Value "$env:MASTER_KEY"
+New-Variable Timeout -Scope Global -Option Constant -Value 3600
+New-Variable PartitionCount -Scope Global -Option Constant -Value 10
 
-# Write-Host "Starting Emulator"
-# Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key $Key -Timeout $Timeout -PartitionCount $PartitionCount
+Write-Host "Starting Emulator"
+Start-CosmosDbEmulator -AllowNetworkAccess -NoFirewall -NoUI -Key $Key -Timeout $Timeout -PartitionCount $PartitionCount
 
-# Write-Host "Current Emulator Status"
-# Get-CosmosDbEmulatorStatus
+Write-Host "Current Emulator Status"
+Get-CosmosDbEmulatorStatus
 
-# Write-Host "Launching Cosmos Client"
-# node index.js
+Write-Host "Launching Cosmos Client"
+node index.js
 
 # Storage Emulator
 
@@ -103,6 +103,6 @@ node storage_index.js
 Start-Sleep -s 60
 
 Write-Host "Launching Queue Client"
-node storage_index.js
+node queue_index.js
 
 Start-Sleep -s 60
