a361f6afc986cecb2fd1dbf4cc9367630d5e5ace
Plamen Ivanov <plamen.ivanov@jet.com>
2020-11-16 13:53:16 -0500

    trying a different Start-Process pattern

--- launch.ps1
+++ launch.ps1
@@ -37,7 +37,8 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Write-Host "curl SqlLocalDB.MSI"
 curl 'https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SqlLocalDB.MSI' -o .\SqlLocalDB.MSI
 Write-Host "install SqlLocalDB.MSI"
-Start-Process -Wait msiexec -ArgumentList "/i",".\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES" -RedirectStandardOutput "Output.txt" -RedirectStandardError "OutputError.txt"
+Start-Process -Wait \SqlLocalDB.MS -ArgumentList "/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES"
+# Start-Process -Wait msiexec -ArgumentList "/i",".\SqlLocalDB.MSI","/qn","IACCEPTSQLLOCALDBLICENSETERMS=YES" -RedirectStandardOutput "Output.txt" -RedirectStandardError "OutputError.txt"
 
 Start-Sleep -s 60
 
